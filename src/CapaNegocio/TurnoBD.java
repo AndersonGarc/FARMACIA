b45e18d38bd1eee3ac592e533b43de33581f14bb
agarciad@isanagusti.edu.pe <agarciad@isanagustin.edu.pe>
2023-09-16 12:33:38 -0500

    cree la condicion ReportarTurno

--- src/CapaNegocio/TurnoBD.java
+++ src/CapaNegocio/TurnoBD.java
@@ -1,14 +1,44 @@
-  /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CapaNegocio;
 
-/**
- *
- * @author labor
- */
+import CapaConexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 public class TurnoBD {
-    
+
+    private Conexion mysql = new Conexion();
+    private Connection cn = mysql.conectar();
+    private String sql;
+
+    public DefaultTableModel reportarTurno() {
+        DefaultTableModel tabla_temporal;
+        String[] titulos = {"CODIGO", "DESCRIPCION", "INICIO", "FIN", "USUARIO"};
+        String[] registros = new String[5];
+        tabla_temporal = new DefaultTableModel(null, titulos);
+        sql = "select uDni,uNombre,uApellidos,uDireccion,uClave,uCelular,idtipoUsuario,tienda from usuario as u \n"
+                + "inner join tipousuario as tu on u.tp_idtipoUsuario=tu.idtipoUsuario";
+
+        try {
+            PreparedStatement pst = cn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                registros[0] = rs.getString("idturno");
+                registros[1] = rs.getString("descripcion");
+                registros[2] = rs.getString("inicio");
+                registros[3] = rs.getString("fin");
+                registros[4] = rs.getString("u_uDni");
+
+                tabla_temporal.addRow(registros);
+            }
+            return tabla_temporal;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, "Error al reportar Turno BD", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+
+    }
 }
